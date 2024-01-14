2ac99c3a1d74b29d71221314467d72a26503a8d7
this-is-emon <mofijul.islam.emon@gmail.com>
2024-01-14 14:29:19 +0600

    Redoing App.jsx

--- toDo/src/App.jsx
+++ toDo/src/App.jsx
@@ -2,13 +2,23 @@ import "./App.css";
 import AddTodo from "./components/AddTodo/AddTodo";
 import TaskList from "./components/TaskList/TaskList";
 
+let nextId = 3;
+const initialTodos = [
+  { id: 0, title: "Buy milk", done: true },
+  { id: 1, title: "Eat tacos", done: false },
+  { id: 2, title: "Brew tea", done: false },
+];
+
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
   return (
     <>
-      <h1>Holder of AddTodo and TaskList:</h1>
-      <AddTodo />
-      <br />
-      <TaskList />
+      <AddTodo onAddTodo={handleAddTodo} />
+      <TaskList
+        todos={todos}
+        onChangeTodo={handleChangeTodo}
+        onDeleteTodo={handleDeleteTodo}
+      />
     </>
   );
 }

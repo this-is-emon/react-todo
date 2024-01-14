b8feb04d32e4e85a2e43f7fbe36c55d27818350e
this-is-emon <mofijul.islam.emon@gmail.com>
2024-01-14 14:44:29 +0600

    redoing Task comp and fixing eslint bug

--- toDo/src/components/Task/Task.jsx
+++ toDo/src/components/Task/Task.jsx
@@ -1,12 +1,50 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
+import { useState } from "react";
 
-function Task() {
+// eslint-disable-next-line react/prop-types
+export default function Task({ todo, onChange, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false);
+  let todoContent;
+  if (isEditing) {
+    todoContent = (
+      <>
+        <input
+          // eslint-disable-next-line react/prop-types
+          value={todo.title}
+          onChange={(e) => {
+            onChange({
+              ...todo,
+              title: e.target.value,
+            });
+          }}
+        />
+        <button onClick={() => setIsEditing(false)}>Save</button>
+      </>
+    );
+  } else {
+    todoContent = (
+      <>
+        {/* eslint-disable-next-line react/prop-types */}
+        {todo.title}
+        <button onClick={() => setIsEditing(true)}>Edit</button>
+      </>
+    );
+  }
   return (
-    <>
-      <i>Task Component : Inside TaskList component</i>
-    </>
+    <label>
+      <input
+        type="checkbox"
+        // eslint-disable-next-line react/prop-types
+        checked={todo.done}
+        onChange={(e) => {
+          onChange({
+            ...todo,
+            done: e.target.checked,
+          });
+        }}
+      />
+      {todoContent}
+      {/* eslint-disable-next-line react/prop-types */}
+      <button onClick={() => onDelete(todo.id)}>Delete</button>
+    </label>
   );
 }
-
-export default Task;
--- toDo/src/components/TaskList/TaskList.jsx
+++ toDo/src/components/TaskList/TaskList.jsx
@@ -1,7 +1,9 @@
+import Task from "../Task/Task";
 // eslint-disable-next-line react/prop-types
 export default function TaskList({ todos, onChangeTodo, onDeleteTodo }) {
   return (
     <ul>
+      {/* eslint-disable-next-line react/prop-types */}
       {todos.map((todo) => (
         <li key={todo.id}>
           <Task todo={todo} onChange={onChangeTodo} onDelete={onDeleteTodo} />
